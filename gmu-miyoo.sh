b510038be2c0c284597a28176dfdba1a05d5013c
Apaczer <94932128+Apaczer@users.noreply.github.com>
2024-06-23 21:36:21 +0200

    BUILD.miyoo: add extra OFFSET cflags for LFS
    update `gmu-miyoo.sh`

--- gmu-miyoo.sh
+++ gmu-miyoo.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
-SDL_NOMOUSE=1 LD_LIBRARY_PATH=libs.miyoo/ ./gmu.bin -c gmu.miyoo.conf
+if ! test -e firstrun; then
+    SDL_NOMOUSE=1 LD_LIBRARY_PATH=$(pwd)/libs.miyoo/ ./gmu.bin -c gmu.miyoo.conf
+    touch firstrun
+else
+    SDL_NOMOUSE=1 LD_LIBRARY_PATH=$(pwd)/libs.miyoo/ ./gmu.bin
+fi
